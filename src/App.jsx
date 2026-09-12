2a7266551c83a131a6ef6915c15de633300fab60
Anne van der Aar <annevanderaar@hotmail.com>
2026-09-12 18:57:35 +0200

    Added home base

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home.jsx';
 
 function App() {
 
   return (
     <>
-     <h1>LMTapes</h1>
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
     </>
   )
 }
